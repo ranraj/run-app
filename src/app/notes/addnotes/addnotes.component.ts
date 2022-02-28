96d1e55c5fbb268a4feeea5441560c502ad1662a
ranraj <ranjithraj.d@gmail.com>
2022-02-28 15:31:11 +0530

    Add param to route

--- src/app/notes/addnotes/addnotes.component.ts
+++ src/app/notes/addnotes/addnotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-addnotes',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addnotes.component.css']
 })
 export class AddnotesComponent implements OnInit {
-
-  constructor() { }
+  id?: string;
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+    this.id = this.route.snapshot.paramMap.get('id') ?? "";
+    console.log(this.id);
+   });
   }
 
 }
